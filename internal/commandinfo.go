edb1a80c9eac5f79b33710181c40c572aa0dcb67
hotheaded <41963445+hotheaded@users.noreply.github.com>
2018-10-10 03:11:26 -0400

    Update commandinfo.go

--- internal/commandinfo.go
+++ internal/commandinfo.go
@@ -1,18 +1,14 @@
-// Copyright 2014 Gary Burd
-//
-// Licensed under the Apache License, Version 2.0 (the "License"): you may
+2.0 (the "License"): you may
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// distributed unde an "AS IS" BASIS, WITHOUT
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations
-// under the License.
-
-package internal // import "github.com/gomodule/redigo/internal"
+// under the Liceedigo/internal"
 
 import (
 	"strings"
@@ -25,20 +21,7 @@ const (
 	MonitorState
 )
 
-type CommandInfo struct {
-	Set, Clear int
-}
-
-var commandInfos = map[string]CommandInfo{
-	"WATCH":      {Set: WatchState},
-	"UNWATCH":    {Clear: WatchState},
-	"MULTI":      {Set: MultiState},
-	"EXEC":       {Clear: WatchState | MultiState},
-	"DISCARD":    {Clear: WatchState | MultiState},
-	"PSUBSCRIBE": {Set: SubscribeState},
-	"SUBSCRIBE":  {Set: SubscribeState},
-	"MONITOR":    {Set: MonitorState},
-}
+type CommandInf
 
 func init() {
 	for n, ci := range commandInfos {
